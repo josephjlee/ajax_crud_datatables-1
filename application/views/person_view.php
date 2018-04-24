e0fd33fa5807d696a6f1753a0d9302d109f3e453
mamat08nurahmat <mamat08nurahmat@gmail.com>
2018-04-24 23:38:39 +0700

    ci_ajax_crud_datatable_image

--- application/views/person_view.php
+++ application/views/person_view.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <head> 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ajax CRUD with Bootstrap modals and Datatables</title>
+    <title>Ajax CRUD with Bootstrap modals and Datatables with Image Upload</title>
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.min.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css')?>" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    </head>
+    </head> 
 <body>
     <div class="container">
-        <h1 style="font-size:20pt">Ajax CRUD with Bootstrap modals and Datatables</h1>
+        <h1 style="font-size:20pt">Ajax CRUD with Bootstrap modals and Datatables with Image Upload</h1>
 
         <h3>Person Data</h3>
         <br />
@@ -25,7 +25,6 @@
         <button class="btn btn-default" onclick="reload_table()"><i class="glyphicon glyphicon-refresh"></i> Reload</button>
         <br />
         <br />
-
         <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
                 <tr>
@@ -34,7 +33,8 @@
                     <th>Gender</th>
                     <th>Address</th>
                     <th>Date of Birth</th>
-                    <th style="width:125px;">Action</th>
+                    <th>Photo</th>
+                    <th style="width:150px;">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -47,6 +47,7 @@
                 <th>Gender</th>
                 <th>Address</th>
                 <th>Date of Birth</th>
+                <th>Photo</th>
                 <th>Action</th>
             </tr>
             </tfoot>
@@ -56,7 +57,7 @@
 <script src="<?php echo base_url('assets/jquery/jquery-2.1.4.min.js')?>"></script>
 <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
 <script src="<?php echo base_url('assets/datatables/js/jquery.dataTables.min.js')?>"></script>
-<script src="<?php echo base_url('assets/datatables/js/dataTables.bootstrap.js')?>"></script>
+<script src="<?php echo base_url('assets/datatables/js/dataTables.bootstrap.min.js')?>"></script>
 <script src="<?php echo base_url('assets/bootstrap-datepicker/js/bootstrap-datepicker.min.js')?>"></script>
 
 
@@ -64,11 +65,12 @@
 
 var save_method; //for save method string
 var table;
+var base_url = '<?php echo base_url();?>';
 
 $(document).ready(function() {
 
     //datatables
-    table = $('#table').DataTable({
+    table = $('#table').DataTable({ 
 
         "processing": true, //Feature control the processing indicator.
         "serverSide": true, //Feature control DataTables' server-side processing mode.
@@ -82,10 +84,14 @@ $(document).ready(function() {
 
         //Set column definition initialisation properties.
         "columnDefs": [
-        {
-            "targets": [ -1 ], //last column
-            "orderable": false, //set not orderable
-        },
+            { 
+                "targets": [ -1 ], //last column
+                "orderable": false, //set not orderable
+            },
+            { 
+                "targets": [ -2 ], //2 last column (photo)
+                "orderable": false, //set not orderable
+            },
         ],
 
     });
@@ -97,7 +103,21 @@ $(document).ready(function() {
         todayHighlight: true,
         orientation: "top auto",
         todayBtn: true,
-        todayHighlight: true,
+        todayHighlight: true,  
+    });
+
+    //set input/textarea/select event when change value, remove class error and remove text help block 
+    $("input").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+    $("textarea").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
+    });
+    $("select").change(function(){
+        $(this).parent().parent().removeClass('has-error');
+        $(this).next().empty();
     });
 
 });
@@ -112,6 +132,10 @@ function add_person()
     $('.help-block').empty(); // clear error string
     $('#modal_form').modal('show'); // show bootstrap modal
     $('.modal-title').text('Add Person'); // Set Title to Bootstrap modal title
+
+    $('#photo-preview').hide(); // hide photo preview modal
+
+    $('#label-photo').text('Upload Photo'); // label photo upload
 }
 
 function edit_person(id)
@@ -121,9 +145,10 @@ function edit_person(id)
     $('.form-group').removeClass('has-error'); // clear error class
     $('.help-block').empty(); // clear error string
 
+
     //Ajax Load data from ajax
     $.ajax({
-        url : "<?php echo site_url('person/ajax_edit/')?>/" + id,
+        url : "<?php echo site_url('person/ajax_edit')?>/" + id,
         type: "GET",
         dataType: "JSON",
         success: function(data)
@@ -138,6 +163,22 @@ function edit_person(id)
             $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
             $('.modal-title').text('Edit Person'); // Set title to Bootstrap modal title
 
+            $('#photo-preview').show(); // show photo preview modal
+
+            if(data.photo)
+            {
+                $('#label-photo').text('Change Photo'); // label photo upload
+                $('#photo-preview div').html('<img src="'+base_url+'upload/'+data.photo+'" class="img-responsive">'); // show photo
+                $('#photo-preview div').append('<input type="checkbox" name="remove_photo" value="'+data.photo+'"/> Remove photo when saving'); // remove photo
+
+            }
+            else
+            {
+                $('#label-photo').text('Upload Photo'); // label photo upload
+                $('#photo-preview div').text('(No photo)');
+            }
+
+
         },
         error: function (jqXHR, textStatus, errorThrown)
         {
@@ -148,13 +189,13 @@ function edit_person(id)
 
 function reload_table()
 {
-    table.ajax.reload(null,false); //reload datatable ajax
+    table.ajax.reload(null,false); //reload datatable ajax 
 }
 
 function save()
 {
     $('#btnSave').text('saving...'); //change button text
-    $('#btnSave').attr('disabled',true); //set button disable
+    $('#btnSave').attr('disabled',true); //set button disable 
     var url;
 
     if(save_method == 'add') {
@@ -164,10 +205,14 @@ function save()
     }
 
     // ajax adding data to database
+
+    var formData = new FormData($('#form')[0]);
     $.ajax({
         url : url,
         type: "POST",
-        data: $('#form').serialize(),
+        data: formData,
+        contentType: false,
+        processData: false,
         dataType: "JSON",
         success: function(data)
         {
@@ -177,9 +222,16 @@ function save()
                 $('#modal_form').modal('hide');
                 reload_table();
             }
-
+            else
+            {
+                for (var i = 0; i < data.inputerror.length; i++) 
+                {
+                    $('[name="'+data.inputerror[i]+'"]').parent().parent().addClass('has-error'); //select parent twice to select div form-group class and add has-error class
+                    $('[name="'+data.inputerror[i]+'"]').next().text(data.error_string[i]); //select span help-block class set text error string
+                }
+            }
             $('#btnSave').text('save'); //change button text
-            $('#btnSave').attr('disabled',false); //set button enable
+            $('#btnSave').attr('disabled',false); //set button enable 
 
 
         },
@@ -187,7 +239,7 @@ function save()
         {
             alert('Error adding / update data');
             $('#btnSave').text('save'); //change button text
-            $('#btnSave').attr('disabled',false); //set button enable
+            $('#btnSave').attr('disabled',false); //set button enable 
 
         }
     });
@@ -229,7 +281,7 @@ function delete_person(id)
             </div>
             <div class="modal-body form">
                 <form action="#" id="form" class="form-horizontal">
-                    <input type="hidden" value="" name="id"/>
+                    <input type="hidden" value="" name="id"/> 
                     <div class="form-body">
                         <div class="form-group">
                             <label class="control-label col-md-3">First Name</label>
@@ -267,6 +319,20 @@ function delete_person(id)
                             <label class="control-label col-md-3">Date of Birth</label>
                             <div class="col-md-9">
                                 <input name="dob" placeholder="yyyy-mm-dd" class="form-control datepicker" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group" id="photo-preview">
+                            <label class="control-label col-md-3">Photo</label>
+                            <div class="col-md-9">
+                                (No photo)
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3" id="label-photo">Upload Photo </label>
+                            <div class="col-md-9">
+                                <input name="photo" type="file">
                                 <span class="help-block"></span>
                             </div>
                         </div>
